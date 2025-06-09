dbdbb84e2f58f3e6fef9180a691198d8b39a2d12
Belen <manzules@est.ups.edu.ec>
2025-06-09 16:46:09 -0500

    Variables y consola

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 //Función para mostrar un cuadro de diálogo emergente
 function mostrarMensaje() {
     alert ("Hola mundo :)");
+}
+
+    //Variables
+function imprimirVariables() {
+    let nombre = "Belen"
+    let edad = 23;
+    //Forma tradicional de concatenar cadenas
+    console.log("nombre: ", nombre);
+    console.log("edad: ", edad);
+    //Forma moderna de concatenar cadenas
+    console.log(`nombre: ${nombre}, edad ${edad}`);
+    //Otros tipos de impresión
+    console.error("Error: xd");
+    console.warn("Advertencia: xd ");
 
 
 }
