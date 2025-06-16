3b1fcfe673455e3f7666176d00fd99bfa02ebba3
Belen <manzules@est.ups.edu.ec>
2025-06-16 15:08:58 -0500

    Actualizado

--- script.js
+++ script.js
@@ -1,36 +1,55 @@
-//Función para mostrar un cuadro de diálogo emergente
-function mostrarMensaje() {
-    alert ("Hola mundo :)");
+// Funcion para mostrar un cuadro de dialogo emergente
+function mostrarMensaje(){
+    alert("Hola mundo :)");
 }
 
-    //Variables
-function imprimirVariables() {
-    let nombre = "Belen"
-    let edad = 23;
-    //Forma tradicional de concatenar cadenas
+// Variables
+function imprimirVariables(){
+    let nombre = "Rodrigo";
+    let edad = 42;
+    // Forma tradicional de concatenar cadenas
     console.log("nombre: ", nombre);
     console.log("edad: ", edad);
-    //Forma moderna de concatenar cadenas
-    console.log(`nombre: ${nombre}, edad ${edad}`);
-    //Otros tipos de impresión
-    console.error("Error: xd");
-    console.warn("Advertencia: xd ");
+    // Forma moderna de concatenar cadenas
+    console.log(`nombre: ${nombre}, edad: ${edad}`);
+    // Otros tipos de impresión
+    console.error("Error: bla bla bla");
+    console.warn("Advertencia: x x x");
 }
-    function arreglos(){
-        //Arreglos
-        let frutas =['manzana', 'pera', 'banana'];
-        //Bucle
-        for (let f of frutas) {
-            console.log(f)
-            
-        }
-        //Condicionales
-        if (frutas[0] === 'pera') {
-            console.log("La segunda fruta es una pera");
-        }else {
-            console.log('No es una pera');
-        }
 
-
-    
+function arreglos(){
+    // Arreglos
+    let frutas = ['manzana', 'pera', 'banana'];
+    // Bucle
+    for(let f of frutas){
+        console.log(f)
+    }
+    // Condicionales
+    if (frutas[1] === 'pera'){
+        console.log("La segunda fruta es una pera");
+    }else{
+        console.log('No es una pera');
+    }
 }
+// Agregar evente a un elemento HTML
+document.getElementById("btnSaludar").addEventListener(
+    "click", function () {
+        console.log("Hola!");
+    }
+);
+
+// Modificar el HTML a través del DOM
+document.getElementById("btnAgregar").addEventListener(
+    "click", () => {
+        // Obtener el elemnto "lista" (ul) del documento HTML
+        let lista = document.getElementById("lista");
+        // Creando un nuevo elemento HTML, li = list item
+        let nuevoItem = document.createElement("li");
+        // Obteniendo el total de ítems de la lista
+        let totalItems = lista.childElementCount;
+        // Agregando un contenido (texto) al item
+        nuevoItem.textContent = `Item ${totalItems + 1}`;
+        // Agregar item a la lista
+        lista.appendChild(nuevoItem);
+    }
+)
